404fa60f8a38da68c0e7724906975ae5dcd94f5e
Harun Rashid <66956330+harundevops-repo@users.noreply.github.com>
2020-09-04 01:46:59 +0530

    Update ThingTest.java

--- src/test/java/com/geekcap/vmturbo/ThingTest.java
+++ src/test/java/com/geekcap/vmturbo/ThingTest.java
@@ -14,12 +14,7 @@ public class ThingTest {
         t.setN(5);
         Assert.assertEquals("N should be 5", 5, t.getN());
     }
-}
-/**
- * Created by Harun on 9/02/2020.
- */
-public class ThingTest2 {
-
+    
     @Test2
     public void testN() {
         Thing t = new Thing();
